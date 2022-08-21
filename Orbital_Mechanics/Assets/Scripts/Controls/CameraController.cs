45b315497c0829be6366bec9daf44b5bfa8dabfa
tomtar00 <80096105+tomtar00@users.noreply.github.com>
2022-08-21 20:32:04 +0200

    added skybox & 3d camera controller

--- Orbital_Mechanics/Assets/Scripts/Controls/CameraController.cs
+++ Orbital_Mechanics/Assets/Scripts/Controls/CameraController.cs
@@ -1,42 +1,85 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float acceleration = 50;       
+    [SerializeField] private float accSprintMultiplier = 4; 
+    [SerializeField] private float sensitivity = 1; 
+    [SerializeField] private float damping = 5; 
+    [SerializeField] private bool focusOnEnable = true; 
 
-    private void Update() {
-        Move();
-        Zoom();
+    Vector3 velocity;
+
+    static bool Focused
+    {
+        get => Cursor.lockState == CursorLockMode.Locked;
+        set
+        {
+            Cursor.lockState = value ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !value;
+        }
     }
 
-    private void Move() {
-        Vector3 diff = Vector3.zero;
-        if (Input.GetKey(KeyCode.W)) {
-            diff += Vector3.forward;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            diff += Vector3.left;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            diff += Vector3.back;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            diff += Vector3.right;
-        }
-        transform.position += diff * Time.unscaledDeltaTime * moveSpeed;
+    void OnEnable()
+    {
+        if (focusOnEnable) Focused = true;
     }
 
-    private void Zoom() {
-        Vector3 diff = Vector3.zero;
-        if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-            diff = Vector3.up;
+    void OnDisable() => Focused = false;
+
+    void Update()
+    {
+        if (Focused)
+            UpdateInput();
+        else if (Input.GetMouseButtonDown(0) && !GUIHoverListener.focusingOnGUI)
+            Focused = true;
+
+        velocity = Vector3.Lerp(velocity, Vector3.zero, damping * Time.deltaTime);
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    void UpdateInput()
+    {
+        // Position
+        velocity += GetAccelerationVector() * Time.deltaTime;
+
+        // Rotation
+        Vector2 mouseDelta = sensitivity * new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+        Quaternion rotation = transform.rotation;
+        Quaternion horizontal = Quaternion.AngleAxis(mouseDelta.x, Vector3.up);
+        Quaternion vertical = Quaternion.AngleAxis(mouseDelta.y, Vector3.right);
+        transform.rotation = horizontal * rotation * vertical;
+
+        // Leave cursor lock
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Focused = false;
+    }
+
+    Vector3 GetAccelerationVector()
+    {
+        Vector3 moveInput = default;
+
+        void AddMovement(KeyCode key, Vector3 dir)
+        {
+            if (Input.GetKey(key))
+                moveInput += dir;
         }
-        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-            diff = Vector3.down;
-        }
-        transform.position += diff * Time.unscaledDeltaTime * zoomSpeed;
+
+        AddMovement(KeyCode.W, Vector3.forward);
+        AddMovement(KeyCode.S, Vector3.back);
+        AddMovement(KeyCode.D, Vector3.right);
+        AddMovement(KeyCode.A, Vector3.left);
+        AddMovement(KeyCode.Space, Vector3.up);
+        AddMovement(KeyCode.LeftControl, Vector3.down);
+        Vector3 direction = transform.TransformVector(moveInput.normalized);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            return direction * (acceleration * accSprintMultiplier);
+        return direction * acceleration;
+    }
+
+    public void FocusOnShip() {
+        GameObject ship = GameObject.FindGameObjectWithTag("Player");
+        transform.SetParent(ship.transform);
     }
 }
---
+++ Orbital_Mechanics/Assets/Scripts/Controls/GUIHoverListener.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class GUIHoverListener : MonoBehaviour
+{
+    public static bool focusingOnGUI { get; private set; }
+    void Update()
+    {
+        focusingOnGUI = EventSystem.current.IsPointerOverGameObject();
+    }
+}
